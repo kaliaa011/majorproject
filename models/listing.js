6019a18f7079240061b1ca63130e717f3c4469ea
VASHISTH <160426080+kaliaa011@users.noreply.github.com>
2024-05-17 11:28:36 +0530

    Add files via upload

---
+++ models/listing.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description : String,
+    // image: {
+    //     type:String,
+    //     set: (v) => 
+    //         v === "https://unsplash.com/photos/a-person-standing-on-top-of-a-mountain-at-sunset-vcTxCvh710A" ? "default link" : v,
+    // }, 
+    price : Number,
+    location : String,
+    country : String,
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
