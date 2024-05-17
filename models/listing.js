18d890d7f6a2511aa404ee2084ee5136b86f7bfc
VASHISTH <160426080+kaliaa011@users.noreply.github.com>
2024-05-17 11:48:10 +0530

    Update listing.js

--- models/listing.js
+++ models/listing.js
@@ -7,11 +7,11 @@ const listingSchema = new Schema({
         required: true,
     },
     description : String,
-    // image: {
-    //     type:String,
-    //     set: (v) => 
-    //         v === "https://unsplash.com/photos/a-person-standing-on-top-of-a-mountain-at-sunset-vcTxCvh710A" ? "default link" : v,
-    // }, 
+    image: {
+        type:String,
+        set: (v) => 
+            v === "https://unsplash.com/photos/a-person-standing-on-top-of-a-mountain-at-sunset-vcTxCvh710A" ? "default link" : v,
+    }, 
     price : Number,
     location : String,
     country : String,
